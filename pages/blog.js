d3158c22758c15af1a6ad8c7809cea8f38f51aba
JoeManuel15 <jsmnlhm@outlook.com>
2023-03-12 18:50:58 -0600

    agregaron componentes del layout, header y fooder

---
+++ pages/blog.js
@@ -0,0 +1,16 @@
+import Layout from "../components/layout"
+
+export default function Blog() {
+  return (
+    <Layout
+      title={'Blog'}
+      description="Blog de música, venta de guitarras, consejos, GuitarLA"
+    >
+        <main className="contenedor">
+            <h1 className="heading">Blog</h1>
+
+            
+        </main>
+    </Layout>
+  )
+}
---
+++ pages/tienda.js
@@ -0,0 +1,16 @@
+import Layout from "../components/layout"
+
+export default function Tienda() {
+  return (
+    <Layout
+      title={'Tienda virtual'}
+      description="Tienda virtual, venta de guitarras, instrumentos, GuitarLA"
+    >
+        <main className="contenedor">
+            <h1 className="heading">tienda</h1>
+
+            
+        </main>
+    </Layout>
+  )
+}
